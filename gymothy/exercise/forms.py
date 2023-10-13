cf473cf1cf036fa95740cdf86f9e41644a8fd2cb
stephenristow <stephenristow@live.com>
2023-10-12 21:50:27 -0700

    Added front end capability to add exercises to workout. In the future, it would be better to split the exercise app into an exercise app and a set app. So, the user would be able to add exercises and sets independently. These are potential next steps after completing user registration and log in. And link each user to their own workouts.

--- gymothy/exercise/forms.py
+++ gymothy/exercise/forms.py
@@ -1,16 +1,19 @@
 from django import forms
 from .models import Exercise
 
-
+WEIGHT_TYPE_CHOICES = [
+    ('lb', 'lb'),
+    ('kg', 'kg'),
+]
     
 class ExerciseForm(forms.ModelForm):
-    exercise_text = forms.CharField(widget=forms.Textarea(attrs={'class': 'input is-medium'}), required=True)
+    exercise_text = forms.CharField(widget=forms.TextInput(attrs={"size": "10"}), required=True)
 
     #weights, sets & reps
-    weight_num = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'input is-medium'}), required=True)
-    weight_type = forms.CharField(widget=forms.Textarea(attrs={'class': 'input is-medium'}), required=True)
-    set_num = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'input is-medium'}), required=True)
-    rep_num = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'input is-medium'}), required=True)
+    weight_num = forms.IntegerField(widget=forms.NumberInput(), required=True)
+    weight_type = forms.CharField(widget=forms.Select(choices=WEIGHT_TYPE_CHOICES), required=True)
+    set_num = forms.IntegerField(widget=forms.NumberInput(), required=True)
+    rep_num = forms.IntegerField(widget=forms.NumberInput(), required=True)
     class Meta:
         model = Exercise
         fields = ('exercise_text', 'weight_num', 'weight_type', 'set_num', 'rep_num',)
