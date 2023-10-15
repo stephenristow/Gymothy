04fcf4ce634de3494d93546dc4c5a61ef1a1c076
stephenristow <stephenristow@live.com>
2023-10-14 23:19:22 -0700

    Added front end for login and user sign up pages. Next steps are fixing bug regarding logging in and adding functionality behind sign up (you cannot actually sign up yet).

--- gymothy/authy/forms.py
+++ gymothy/authy/forms.py
@@ -73,12 +73,10 @@ class ChangePasswordForm(forms.ModelForm):
 
 class EditProfileForm(forms.ModelForm):
 	picture = forms.ImageField(required=False)
-	first_name = forms.CharField(widget=forms.TextInput(), max_length=50, required=False)
-	last_name = forms.CharField(widget=forms.TextInput(), max_length=50, required=False)
-	location = forms.CharField(widget=forms.TextInput(), max_length=25, required=False)
+	height = forms.IntegerField(widget=forms.NumberInput(), required=False)
+	weight = forms.IntegerField(widget=forms.NumberInput(), required=False)
 	url = forms.URLField(widget=forms.TextInput(), max_length=60, required=False)
-	profile_info = forms.CharField(widget=forms.TextInput(), max_length=260, required=False)
 
 	class Meta:
 		model = Profile
-		fields = ('picture', 'first_name', 'last_name', 'location', 'url', 'profile_info')
+		fields = ('picture', 'height', 'weight', 'url')
